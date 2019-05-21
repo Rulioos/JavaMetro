5933aca6f86bb06c6e955bd4e790cbdc46cd6679
jules <jules.aubier97@gmail.com>
2019-05-21 11:04:11 +0200

    adding data classes definition

--- src/projetMetro/Edge.java
+++ src/projetMetro/Edge.java
@@ -1,5 +1,54 @@
 package projetMetro;
 
 public class Edge {
+	private SubwayStation stop1;
+	private SubwayStation stop2;
+	private int time;//travel time
+	
+	/* Constructors*/
+	
+	//For weighed graph
+	public Edge(SubwayStation stop1, SubwayStation stop2, int time) {
+		super();
+		this.stop1 = stop1;
+		this.stop2 = stop2;
+		this.time = time;
+	}
+	//For unweighed graph
+	public Edge(SubwayStation stop1, SubwayStation stop2) {
+		super();
+		this.stop1 = stop1;
+		this.stop2 = stop2;
+	}
+	
+	/*Getters and setters*/
+	
+	public SubwayStation getStop1() {
+		return stop1;
+	}
+
+
+	public void setStop1(SubwayStation stop1) {
+		this.stop1 = stop1;
+	}
+
+	public SubwayStation getStop2() {
+		return stop2;
+	}
+
+	public void setStop2(SubwayStation stop2) {
+		this.stop2 = stop2;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+	
+	
+	
 
 }
--- src/projetMetro/SubwayStation.java
+++ src/projetMetro/SubwayStation.java
@@ -1,5 +1,41 @@
 package projetMetro;
 
 public class SubwayStation {
+	private int stop_id;
+	private String stop_name;
+	private String stop_loc;//stop's address
+	
+	public SubwayStation(int stop_id, String stop_name, String stop_loc) {
+		super();
+		this.stop_id = stop_id;
+		this.stop_name = stop_name;
+	}
 
+	public int getStop_id() {
+		return stop_id;
+	}
+
+	public void setStop_id(int stop_id) {
+		this.stop_id = stop_id;
+	}
+
+	public String getStop_name() {
+		return stop_name;
+	}
+
+	public void setStop_name(String stop_name) {
+		this.stop_name = stop_name;
+	}
+
+	public String getStop_loc() {
+		return stop_loc;
+	}
+
+	public void setStop_loc(String stop_loc) {
+		this.stop_loc = stop_loc;
+	}
+	
+	
+	
+	
 }
